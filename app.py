7d451a7823d2d405dededcc0974241130a65f9a0
juan <jpuerto96@hotmail.com>
2020-05-07 20:48:02 -0400

    App: Modify import statement

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-import endpoints
+from endpoints.UsersAPI import users_endpoints
 
 import logging
 
