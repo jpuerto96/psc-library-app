f8c4d7609402fe7d4ed8c79c87a5d4e52e21cb64
juan <jpuerto96@hotmail.com>
2020-05-09 18:14:16 -0400

    BooksAPI: Add REST endpoints for interaction with book entries

---
+++ endpoints/api_routes/booksapi.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, url_for, render_template, request, current_app, Response
+import json
+
+from app import db
+from models.booksmodel import BooksModel
+
+books_api_endpoints = Blueprint('books_api_endpoints', __name__)
+
+
+@books_api_endpoints.route('/books/', methods=['GET', 'POST'])
+@books_api_endpoints.route('/books/<book_id>', methods=['GET', 'PUT'])
+def book(book_id=None):
+    if request.method == 'GET':
+        return json.dumps(BooksModel.query.get(int(book_id)) if book_id
+                          else BooksModel.query.filter_by(request.get_json()).all())
+    elif request.method == 'POST':
+        body = request.get_json(force=True)
+        book_object = BooksModel(**body)
+        db.session.add(book_object)
+        db.session.commit()
+        return json.dumps(book_object.serialize())
+    elif request.method == 'PUT':
+        body = request.get_json(force=True)
+        book_object = BooksModel.query.get(int(book_id))
+        for key, value in book_object.iteritems():
+            if body[key]:
+                setattr(book_object, key, body[key])
+        db.session.commit()
+        return json.dumps(book_object.serialize)
+    else:
+        return Response(status=404)
