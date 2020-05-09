f57b579236b5305513db1da4545692defb5804ed
juan <jpuerto96@hotmail.com>
2020-05-09 13:11:13 -0400

    BooksModel: Remove columns

--- models/booksmodel.py
+++ models/booksmodel.py
@@ -6,17 +6,13 @@ class BookModel(db.Model):
 
     id = db.Column(db.Integer,
                    primary_key=True)
+    title = db.Column(db.String(200),
+                      index=True,
+                      unique=True,
+                      nullable=False)
     author = db.Column(db.String(64),
                        index=True,
                        unique=False,
                        nullable=False)
-    date_of_purchase = db.Column(db.Date,
-                                 index=False,
-                                 unique=False,
-                                 nullable=False)
-    notes = db.Column(db.Text,
-                      index=False,
-                      unique=False,
-                      nullable=True)
 
     users = db.relationship("UserBooksModel", back_populates="book")
