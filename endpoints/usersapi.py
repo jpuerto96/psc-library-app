b7f1bd0d119ff0c6eb58b5ffaa69070adfa1f58e
juan <jpuerto96@hotmail.com>
2020-05-09 09:09:40 -0400

    UsersAPI: Update redirects. Update import statements. Update constructor.

--- endpoints/usersapi.py
+++ endpoints/usersapi.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, redirect, url_for, render_template, request, flash
+from flask import Blueprint, redirect, url_for, render_template, request, flash, current_app
 from flask_login import current_user, login_required, login_user, logout_user
 
 from app import db
-from models import UserModel, Forms
+from models.forms import UserLoginForm, UserSignupForm
+from models.usersmodel import UserModel
 
 users_endpoints = Blueprint('users_endpoints', __name__)
 
@@ -17,18 +18,18 @@ def home():
 @users_endpoints.route('/login/', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('users_endpoints.home'))
 
-    login_form = Forms.UserLoginForm()
+    login_form = UserLoginForm()
 
     if login_form.validate_on_submit():
         user = UserModel.query.filter_by(username=login_form.username.data).first()  # Validate Login Attempt
         if user and user.is_valid_password(password=login_form.password.data):
             login_user(user)
             next_page = request.args.get('next')
-            return redirect(next_page or url_for('home'))
+            return redirect(next_page or url_for('users_endpoints.home'))
         flash('Invalid username/password combination')
-        return redirect(url_for('home'))
+        return redirect(url_for('users_endpoints.home'))
 
     return render_template('login.html', form=login_form)
 
@@ -36,29 +37,28 @@ def login():
 @users_endpoints.route('/logout/', methods=['GET'])
 def logout():
     logout_user()
-    return redirect((url_for('login')))
+    return redirect((url_for('users_endpoints.login')))
 
 
 @users_endpoints.route('/signup/', methods=['GET', 'POST'])
 def signup():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('users_endpoints.home'))
 
-    signup_form = Forms.UserSignupForm()
+    signup_form = UserSignupForm()
 
     if signup_form.validate_on_submit():
         existing_user = UserModel.query.filter_by(email=signup_form.email.data).first()
         if existing_user is None:
             user = UserModel(
-                name=signup_form.name.data,
-                email=signup_form.email.data,
-                website=signup_form.website.data
+                username=signup_form.username.data,
+                email=signup_form.email.data
             )
             user.generate_password_hash(signup_form.password.data)
             db.session.add(user)
             db.session.commit()  # Create new user
             login_user(user)  # Log in as newly created user
-            return redirect(url_for('home'))
+            return redirect(url_for('users_endpoints.home'))
         flash('Email address is already registered.')
 
     return render_template('signup.html', form=signup_form)
