ab4992bfe4f773f9277e18d0a931c2f6070e70e6
juan <jpuerto96@hotmail.com>
2020-05-10 15:32:38 -0400

    API: Fix PUT method for books

--- endpoints/api_routes/booksapi.py
+++ endpoints/api_routes/booksapi.py
@@ -22,13 +22,12 @@ def book(book_id=None):
         db.session.commit()
         return json.dumps(book_object.serialize())
     elif request.method == 'PUT':
-        # TODO: Fix PUT method
         body = request.get_json(force=True)
         book_object = BooksModel.query.get(int(book_id))
-        for key, value in book_object.iteritems():
-            if body[key]:
+        for key, value in book_object.serialize().items():
+            if key in body:
                 setattr(book_object, key, body[key])
         db.session.commit()
-        return json.dumps(book_object.serialize)
+        return json.dumps(book_object.serialize())
     else:
         return Response(status=404)
