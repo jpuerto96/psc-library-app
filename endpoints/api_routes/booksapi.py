fc725045bc69d6559fc26d8622d9922762cac8e4
juan <jpuerto96@hotmail.com>
2020-05-10 14:15:58 -0400

    BooksAPI: Add @login_required

--- endpoints/api_routes/booksapi.py
+++ endpoints/api_routes/booksapi.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, url_for, render_template, request, current_app, Response
+from flask_login import current_user, login_required
 import json
 
 from app import db
@@ -9,6 +10,7 @@ books_api_endpoints = Blueprint('books_api_endpoints', __name__)
 
 @books_api_endpoints.route('/books/', methods=['GET', 'POST'])
 @books_api_endpoints.route('/books/<book_id>', methods=['GET', 'PUT'])
+@login_required
 def book(book_id=None):
     if request.method == 'GET':
         return json.dumps(BooksModel.query.get(int(book_id)) if book_id
@@ -20,6 +22,7 @@ def book(book_id=None):
         db.session.commit()
         return json.dumps(book_object.serialize())
     elif request.method == 'PUT':
+        # TODO: Fix PUT method
         body = request.get_json(force=True)
         book_object = BooksModel.query.get(int(book_id))
         for key, value in book_object.iteritems():
