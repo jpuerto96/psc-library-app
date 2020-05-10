5607a4157f5fdc809418fc3cc3d32d25ec99a832
juan <jpuerto96@hotmail.com>
2020-05-10 09:49:06 -0400

    AppView: Add functionality to grab user books list

--- endpoints/view_routes/appview.py
+++ endpoints/view_routes/appview.py
@@ -1,10 +1,41 @@
-from flask import Blueprint, url_for, render_template, request, current_app
-from flask_login import login_required, login_user, logout_user
-from flask_mail import Message
+from flask import Blueprint, render_template, current_app
+from flask_login import login_required, current_user
+from models.userbooksmodel import UserBooksModel
+from models.booksmodel import BooksModel
+from app import db
+
+import json
 
 app_view_endpoints = Blueprint('app_view_endpoints', __name__)
 # Return the home page
 @app_view_endpoints.route('/', methods=['GET'])
 @login_required
 def home():
-    return render_template("index.html")
+    user_books_list = UserBooksModel.query.join(BooksModel)\
+        .add_columns(BooksModel.title, BooksModel.author,
+                     UserBooksModel.date_of_purchase, UserBooksModel.notes, UserBooksModel.is_favorite)\
+        .filter(UserBooksModel.user_id == current_user.id).all()
+    return render_template("index.html", book_list=user_books_list)
+
+
+@app_view_endpoints.route('/modal/user_books/<modal_type>/<userbooks_id>', methods=['GET'])
+@login_required
+def user_books_modal(modal_type, userbooks_id):
+    modal_template = json.load(open('./static/json_templates/modal_templates.json'))
+    user_books_template = modal_template['user_books_modal']
+
+    userbook = UserBooksModel.query.join(BooksModel)\
+        .add_columns(BooksModel.title, BooksModel.author,
+                     UserBooksModel.date_of_purchase, UserBooksModel.notes, UserBooksModel.is_favorite)\
+        .filter(UserBooksModel.id == userbooks_id).first() if modal_type == "edit" else {}
+
+    user_books_template['author']['options'] = []
+    for author in db.session.query(BooksModel.author).distinct():
+        user_books_template['author']['options'].append(author.author)
+
+    user_books_template['title']['options'] = []
+    for title in db.session.query(BooksModel.title).distinct():
+        user_books_template['title']['options'].append(title.title)
+
+    return render_template("modals/userbooks_modal.html",
+                           modal_data=userbook, modal_type=modal_type, modal_template=user_books_template)
