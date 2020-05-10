8ce6cc3155d99ce7ecc9165abc62153c52c82fff
juan <jpuerto96@hotmail.com>
2020-05-10 14:16:47 -0400

    UserBooksAPI: POST and DELETE are functioning

--- endpoints/api_routes/userbooksapi.py
+++ endpoints/api_routes/userbooksapi.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, url_for, render_template, request, current_app, Response
+from flask_login import current_user, login_required
 import json
 
 from app import db
@@ -9,18 +10,20 @@ user_books_api_endpoints = Blueprint('user_books_api_endpoints', __name__)
 
 @user_books_api_endpoints.route('/user_books/', methods=['GET', 'POST'])
 @user_books_api_endpoints.route('/user_books/<user_book_id>', methods=['GET', 'PUT', 'DELETE'])
+@login_required
 def user_book(user_book_id=None):
     if request.method == 'GET':
         return json.dumps(UserBooksModel.query.get(int(user_book_id)) if user_book_id
                           else UserBooksModel.query.filter_by(request.get_json()).all())
     elif request.method == 'POST':
         body = request.get_json(force=True)
-        current_app.logger.error(body)
+        body['user_id'] = current_user.id
         user_book_object = UserBooksModel(**body)
         db.session.add(user_book_object)
         db.session.commit()
         return json.dumps(user_book_object.serialize())
     elif request.method == 'PUT':
+        # TODO: Fix PUT method
         body = request.get_json(force=True)
         user_book_object = UserBooksModel.query.get(int(user_book_id))
         for key, value in user_book_object.iteritems():
@@ -29,6 +32,9 @@ def user_book(user_book_id=None):
         db.session.commit()
         return json.dumps(user_book_object.serialize)
     elif request.method == 'DELETE':
-        return json.dumps({"success": True})
+        user_book_object = UserBooksModel.query.get(int(user_book_id))
+        db.session.delete(user_book_object)
+        db.session.commit()
+        return json.dumps({'success': True})
     else:
         return Response(status=404)
