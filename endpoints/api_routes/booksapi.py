bff48cf753d516ba7243f74724d99c55e7921f27
juan <jpuerto96@hotmail.com>
2020-05-10 23:27:38 -0400

    API: Add comments.

--- endpoints/api_routes/booksapi.py
+++ endpoints/api_routes/booksapi.py
@@ -13,13 +13,19 @@ books_api_endpoints = Blueprint('books_api_endpoints', __name__)
 @books_api_endpoints.route('/books/<book_id>', methods=['GET', 'PUT'])
 @login_required
 def book(book_id=None):
+    """
+    REST function for interacting with the books table.
+    """
     if request.method == 'GET':
         return json.dumps(BooksModel.query.get(int(book_id)))
     elif request.method == 'POST':
         body = request.get_json(force=True)
+        # One should check to see whether the book exists already.
         existing_book_object = BooksModel.query.filter_by(**body).first()
         if existing_book_object:
+            # If the book exists, return that book record
             return json.dumps(existing_book_object.serialize())
+        # Otherwise, construct a new book, insert it, and return it.
         book_object = BooksModel(**body)
         db.session.add(book_object)
         db.session.commit()
@@ -27,6 +33,7 @@ def book(book_id=None):
     elif request.method == 'PUT':
         body = request.get_json(force=True)
         book_object = BooksModel.query.get(int(book_id))
+        # Iterate over existing values and update based on body of request.
         for key, value in book_object.serialize().items():
             if key in body:
                 setattr(book_object, key, body[key])
